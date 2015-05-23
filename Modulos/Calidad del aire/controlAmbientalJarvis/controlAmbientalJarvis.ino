ad6f4a439e9ca35b13a1751337a2a4a1b723e9f2
trastejant <nano@trastejant.com>
2015-05-23 20:45:05 +0200

    debugger mode, disable mode

--- Modulos/Calidad del aire/controlAmbientalJarvis/controlAmbientalJarvis.ino
+++ Modulos/Calidad del aire/controlAmbientalJarvis/controlAmbientalJarvis.ino
@@ -7,23 +7,33 @@
 #include <SoftwareSerial.h>
 #include <MQ135.h>
 #include "DHT.h"
+#include <Wire.h>  
+#include "RTClib.h"  
+#include <SD.h>  
+RTC_DS1307 RTC;  
 
 //contantes
-#define luminoso 13
-#define resetPin 12
-#define BTtx 11
-#define BTrx 10
-#define acustico 9
-#define DHTPIN 8 //Seleccionamos el pin en el que se //conectará el sensor
-#define flamepin 3 //Llamas
+#define SCLK 13//SCLK del puerto SPI
+#define MISO 12//MISO del puerto SPI
+#define MOSI 11//MOSI del puerto SPI
+#define CS 10//CS del puerto SPI
+#define luminoso 9//Led para señales visuales
+#define resetPin 8//Pin para activar el reset de Arduino
+#define BTtx 7//TX del puerto serial emulado
+#define BTrx 6//RX del puerto serial emulado
+#define acustico 5//Altavoz piezolectrico para señales acusticas
+#define DHTPIN 4 //Pin del sensor de humedad y temperatura
+#define flamepin 3 //Sensor de Llamas
 #define WAKEUPpin 2//Pin de interrupcion para despertar al Arduino
-#define RX 1
-#define TX 0
-#define MQ135PIN A0 //NH3, Benceno, Alcohol
-#define LDRpin A1 //Luz
-#define MQ811pin A2 //CO2
-#define MQ07pin A3//CO
-#define DHTTYPE DHT22 //Se selecciona el modelo del DHT
+#define RX 1//RX del puerto serial
+#define TX 0//TX del puerto serial
+#define MQ135PIN A0 //Pin del sensor NH3, Benceno, Alcohol
+#define LDRpin A1 //Pin del sensor de Luz
+#define MQ811pin A2 //Pin del sensor de CO2
+#define MQ07pin A3//Pin del sensor de CO
+#define SDA A4//SDA del I2C
+#define SCL A5//SCL del I2C
+#define DHTTYPE DHT22 //Configuracion del modelo de sensor DHT22
 
 //Variables 
 float temp, humedad;
@@ -31,6 +41,7 @@ int CO, COO;
 boolean humo, llamas, debuggear;
 int address;//direccion del modulo
 String name; //Nombre del modulo
+File myFile;  
 
 //Declaracion de objetos
 SoftwareSerial BT(10, 11); //virtualizamos un puerto serial para la conectividad Bluetooth (RX, TX)
@@ -39,15 +50,19 @@ DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino p
 
 void setup(){
    
-   Serial.begin(57600);//Inicializacion del puerto serial para debugger
-   Serial.println("Serial Debugger Port Open"); 
+   //Serial.begin(57600);//Inicializacion del puerto serial para debugger
+   //Serial.println("Serial Debugger Port Open"); 
    
    BT.begin(4800);//Inicializacion del puerto serial BT
    BT.println("Serial Port Open"); 
    
    dht.begin(); //Se inicia el sensor de temperatura y humedad
    BT.println("init DHT"); 
- 
+   
+   Wire.begin();
+   RTC.begin();//Inicializamos el módulo reloj  
+   BT.println("Real clock init"); 
+   
    // inicializacion de las variables
    temp = -273;
    humedad = -1;
@@ -121,14 +136,53 @@ void sendArray(float lecturas[]){
   delay(10);  
 }
 //Modulo de debugger
-void debuggerMode(){
-  Serial.print("Temperatura");
-  Serial.println(temp);
-  Serial.print("CO");
-  Serial.println(CO);
-  Serial.print("CO2");
-  Serial.println(COO);
-  Serial.print("humo detectado");
-  Serial.println(humo);
-  delay(5000);
+void debuggerMode(){    
+    unsigned long t0 = millis();    // Se toma el tiempo actual  
+    Serial.print("Temperatura");
+    Serial.println(getTemp());
+    Serial.print("Humedad");
+    Serial.print(getHum());
+    Serial.println("%");
+    Serial.print("CO");
+    Serial.println(getMQ135(getTemp(), getHum()));
+    Serial.print("CO2");
+    Serial.println();
+    Serial.print("CO");
+    Serial.println();
+    Serial.print("Deteccion de llamas:");
+    Serial.println(llama());
+    unsigned long t1 = millis();    // Se toma el tiempo actual
+    Serial.print("Todo medido en: ");
+    Serial.print(t1-t0);
+    Serial.println("ms");  
 }
+
+//Activar el modo de debuggeo
+void disableDebuggerMode(){
+  Serial.end();//Cerramos la conexion y liberamos los pines RX y TX
+  digitalWrite(RX,HIGH);//cambia el estado del biestable
+  digitalWrite(RX,LOW);
+  debuggear = false;  
+}
+
+void enableDebuggerMode(){
+  debuggear = true;
+  Serial.begin(9600);//Inicializacion del puerto serial para debugger
+  Serial.println("Serial Debugger Port Open"); 
+}
+
+boolean externalVcc(){
+  if(digitalRead(TX)==HIGH){return true;}
+  else{return false;}
+}
+/*
+void saveData(float datos[]){
+  //Guardamos el registro de datos en la SD
+  DateTime now = RTC.now();//Creamos un objeto que contiene la hora y fecha actual    
+  //String ultimosRegistros = now.day() + "/" + now.month() + "/" + now.year() + " " +now.hour()+":"+now.minute()+":"+now.second()
+  //if(!guardaDatos(ultimosRegistros,"registro.txt")){guardaDatos(datos[],"log.txt");}
+}
+
+boolean guardaDatos(float informacion[], String fichero){return true;}
+void volcarDatos(){}
+*/
