b5afdc49110c618d9259cb6f76bf569a50e0c748
trastejant <nano@trastejant.com>
2015-05-26 10:01:31 +0200

    Interfaz Jarvis

--- Modulos/Calidad del aire/controlAmbientalJarvis/controlAmbientalJarvis.ino
+++ Modulos/Calidad del aire/controlAmbientalJarvis/controlAmbientalJarvis.ino
@@ -1,5 +1,3 @@
-
-
 //Version 0.1 del control ambiental de Jarvis.
 //Desarrollado por CrakerNano 4 Makespace Madrid
 
@@ -14,6 +12,8 @@
 #include "RTClib.h"  
 #include <SD.h>  
 RTC_DS1307 RTC;  
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 //contantes
 #define SCLK 13//SCLK del puerto SPI
@@ -26,7 +26,7 @@ RTC_DS1307 RTC;
 #define BTrx 6//RX del puerto serial emulado
 #define acustico 5//Altavoz piezolectrico para señales acusticas
 #define DHTPIN 4 //Pin del sensor de humedad y temperatura
-#define flamepin 3 //Sensor de Llamas
+#define FLAMEpin 3 //Sensor de Llamas
 #define WAKEUPpin 2//Pin de interrupcion para despertar al Arduino
 #define RX 1//RX del puerto serial y tigger del modo ahorro de energia
 #define TX 0//TX del puerto serial
@@ -38,7 +38,7 @@ RTC_DS1307 RTC;
 #define SCL A5//SCL del I2C
 #define DHTTYPE DHT22 //Configuracion del modelo de sensor DHT22
 
-//Variables 
+//Declaracion de Variables 
 
 boolean debuggear;
 int address;//direccion del modulo
@@ -55,7 +55,7 @@ void setup(){
    //Serial.begin(57600);//Inicializacion del puerto serial para debugger
    //Serial.println("Serial Debugger Port Open"); 
    
-   BT.begin(4800);//Inicializacion del puerto serial BT
+   BT.begin(9600);//Inicializacion del puerto serial BT
    BT.println("Serial Port Open"); 
    
    dht.begin(); //Se inicia el sensor de temperatura y humedad
@@ -68,15 +68,18 @@ void setup(){
    // inicializacion de las variables
    debuggear = false; 
    Serial.println("init variable");
+   
+   //Iniciamos el control de interrupciones
+   attachInterrupt(WAKEUPpin, wakeup, CHANGE);  
+   attachInterrupt(FLAMEpin, llamas, RISING);  
  
 }
 
 void loop(){
-  //Si se ha establecido una conexion Bluetooth, enviamos las lecturas
-  
+  //Si se ha establecido una conexion Bluetooth, enviamos las lecturas  
   if (BT.available()){
-    float lecturas[] = {getHum(), getTemp(), getLight(), llama()}; 
-    sendArray(lecturas);    
+    char c = BT.read();
+    jarvisInterface(c);        
   }  
   
   if(debuggear){debuggerMode;}//Si esta activo el modo de debuggeo llamamos a la funcion
@@ -86,7 +89,7 @@ void loop(){
 
 boolean checkExcLimit(int lectura, int limite){if(lectura>limite)return true;}//Comprueba si el valor pasado supera el limite
 int getLight(){return analogRead(LDRpin);}//Funcion que devuelve la lectura de la intensidad de luz.
-boolean llama(){return digitalRead(flamepin);}//Funcion que devuelve la lectura del sensor de llamas.
+boolean getFlame(){return digitalRead(FLAMEpin);}//Funcion que devuelve la lectura del sensor de llamas.
 float getTemp(){return dht.readTemperature();}//devuelve la temperatura medida
 float getHum(){return dht.readHumidity();} //devuelve la humedad medida
 void Reset(){digitalWrite(resetPin,HIGH);}//Funcion para resetear la placa de Arduino
@@ -149,7 +152,7 @@ void debuggerMode(){
     Serial.print("CO");
     Serial.println();
     Serial.print("Deteccion de llamas:");
-    Serial.println(llama());
+    Serial.println(getFlame());
     unsigned long t1 = millis();    // Se toma el tiempo actual
     Serial.print("Todo medido en: ");
     Serial.print(t1-t0);
@@ -170,14 +173,18 @@ void enableDebuggerMode(){
   Serial.println("Serial Debugger Port Open"); 
 }
 
+boolean debuggerStatusMode(){return debuggear;} 
+
 //Devuelve TRUE si hay alimentacion externa y FALSE en caso contrario
 boolean externalVcc(){
   if(digitalRead(TX)==HIGH){return true;}
   else{return false;}
 }
+void enableExternalVcc(){digitalWrite(RX,HIGH);}
+void disableExternalVcc(){digitalWrite(RX,LOW);}
 
 //Devuelve el porcentaje de luz etectado
-float getLight(int LDRpin){
+float getLightPerCent(int LDRpin){
    int light = analogRead(LDRpin);
    float lightPerCent = light/10.23;//calculamos el tanto por ciento de luz detectada
    return lightPerCent;
@@ -201,7 +208,7 @@ void saveData(float datos[], String fichero){
   if (!SD.begin(CS)) {
     myFile = SD.open("registros.txt", FILE_WRITE);
     if (myFile) {
-      myFile.print(date() + " - " + time() + " - ");  //Marcamos fecha y hora en el registro
+      myFile.print(getDate() + " - " + getTime() + " - ");  //Marcamos fecha y hora en el registro
       for(int i=0; i<sizeof(datos)/sizeof(float); i++){
         myFile.print(datos[i]);
         myFile.print(",");
@@ -216,7 +223,7 @@ void saveLog(String msg){
   if (!SD.begin(CS)) {
     myFile = SD.open("log.txt", FILE_WRITE);
     if (myFile) {
-      myFile.print(date() + " - " + time() + " - ");  //Marcamos fecha y hora en el registro
+      myFile.print(getDate() + " - " + getTime() + " - ");  //Marcamos fecha y hora en el registro
       myFile.println(msg);
     }else{alarm(10);}
   }else{alarm(10);}
@@ -224,12 +231,39 @@ void saveLog(String msg){
 }
 
 //Vuelca el contenido de la tarjeta SD a la conexion
-void download(){
-  
+void downloadSD(){
+  myFile = SD.open("registros.txt");   
+  if (myFile) {
+    //leemos los datos del fichero   
+    while (myFile.available()) {BT.write(myFile.read());}  
+    myFile.close();// cerramos el fichero  
+  }else {Serial.println("opening error");}// Si el fichero no puede abrirse, mostramos un error.
+}
+
+
+//Muestra el contenido del fichero de log
+void showLog(){
+  myFile = SD.open("log.txt");  
+  if (myFile) {
+    //leemos los datos del fichero   
+    while (myFile.available()) {BT.println(myFile.read());}  
+    myFile.close();// cerramos el fichero  
+  }else {Serial.println("opening error");}// Si el fichero no puede abrirse, mostramos un error.  
+
+}
+
+//Vuelva el contenido del fichero de log
+void downloadSDlog(){
+  myFile = SD.open("log.txt");  
+  if (myFile) {
+    //leemos los datos del fichero   
+    while (myFile.available()) {BT.write(myFile.read());}  
+    myFile.close();// cerramos el fichero  
+  }else {Serial.println("opening error");}// Si el fichero no puede abrirse, mostramos un error.
 }
 
 //Devuelve un String con la fecha en formato dd/mm/aaaa
-String date(){
+String getDate(){
    DateTime now = RTC.now();//Creamos un objeto que contiene la hora y fecha actual      
    String fecha = now.day()+"/";
    fecha = fecha+"/"+now.month();
@@ -238,7 +272,7 @@ String date(){
 }
 
 //Devuelve un String con la hora en formato hh/mm/ss
-String time(){
+String getTime(){
   DateTime now = RTC.now();
   String hora = now.hour() + ":";
    hora = hora + ":" +now.minute();
@@ -338,4 +372,154 @@ void alarm(int alarm){
     
   } 
   
+}
+float* getArrayLecturas(){float lecturas[] = {getHum(), getTemp(), getLight(), getFlame()}; }
+void jarvisInterface(int c){
+  /*
+  0 -> Envia un array con todas las lecturas
+  1 -> Activa la alimentacion externa
+  2 -> Desactiva la alimentacion externa
+  3 -> Devuelve el estado de la alimentacion externa
+  4 -> Devuelva la llectura del sensor de llamas
+  5 -> devuelve la lectura de la humedad
+  6 -> Devuelve la lectura de la temperatura
+  7 -> Prueba del zumbador
+  8 -> Producen un reset en la placa de Arduino
+  9 -> Prueba la alarma visual
+  10 -> Volcado de datos de la tarjeta SD
+  11 -> Lectura de la calidad del aire
+  12 -> Lectura de la cantidad de luz
+  13 -> Lectura del sensor de CO 
+  14 -> Lectura del sensor de COO
+  15 -> Devuelve la hora del modulo
+  16 -> Desabilita la placa
+  17 -> Coloca el Arduino en Sleep  
+  18 -> Activa/Desactiva el modo de debuggeo
+  19 -> Devuelve la MAC del modulo
+  20 -> Asigna una MAC al modulo
+  21 -> Volcado de registro de log
+  
+  */
+  
+  switch(c){
+    case 0:      
+      sendArray(getArrayLecturas());
+      break;
+    case 1:
+      enableExternalVcc();
+      break;
+      
+    case 2:
+      disableExternalVcc();
+      break;
+      
+    case 3:
+      externalVcc();
+      break;
+      
+    case 4:
+      BT.println(getFlame());
+      break;
+      
+    case 5:
+      BT.println(getHum());
+      break;
+      
+    case 6:
+      BT.println(getTemp());
+      break;
+   
+    case 7:
+      soundAlarm(110);
+      break;
+      
+    case 8:
+      Reset();
+      break;
+      
+    case 9:
+      ligthAlarm(500,500);
+      break;
+      
+    case 10:
+      downloadSD();
+      break;
+      
+    case 11:
+      getMQ135(getTemp(),getHum());
+      break;
+      
+    case 12:
+      BT.print(getLight());
+      BT.print(" (");
+      BT.print(getLightPerCent(LDRpin));
+      BT.println(" )");
+      break;
+
+    case 13:
+      BT.println(getCO(MQ07pin));
+      break;
+      
+    case 14:
+      BT.println(getCOO(MQ811pin));
+      break;
+      
+    case 15:
+      BT.println(getDate() +" - "+ getTime());
+      break;
+      
+    case 16:
+      BT.println("¿Cuanto tiempo deseas mantener la placa desabilitada?(s)");
+      int t; 
+      t = BT.read();
+      t = t * 1000;//Convertimos los segundos en milis
+      disable(t);
+      break;
+      
+    case 17:
+      goToSleep();
+      break;
+      
+    case 18:
+      if(debuggerStatusMode()){disableDebuggerMode();}else{enableDebuggerMode();}
+      break;
+   
+    case 19:
+      BT.println(getMAC());
+      break;
+      
+    case 20:
+      BT.println("Escriba la MAC que desea asignar al modulo");
+      uint8_t mac; 
+      mac = BT.read();
+      setMAC(mac);
+      break;
+      
+    case 21:
+      showLog();
+      break;
+      
+    case 22:
+      downloadSDlog();
+      break;
+      
+    case 23:
+    
+      break;
+      
+    case 24:
+    
+      break;
+      
+    default:
+      BT.print("Command not found");
+      break;    
+  }
+}
+
+void llamas(){alarm(0);}
+void wakeup(){sleep_disable();}
+void goToSleep(){
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
+  sleep_enable(); 
 }
